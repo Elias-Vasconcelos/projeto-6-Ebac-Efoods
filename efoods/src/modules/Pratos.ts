90ef8c3cce6d8cb97afd54081764cf87f7c7f1c1
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-07-17 14:15:46 -0300

    Criação da calsse pratos

---
+++ efoods/src/modules/Pratos.ts
@@ -0,0 +1,21 @@
+class Pratos {
+    imagem: string
+    titulo: string
+    descricao: string
+    id: number
+  
+    constructor(
+      imagem: string,
+      titulo: string,
+      descricao: string,
+      id: number
+    ) {
+      this.imagem = imagem
+      this.titulo = titulo
+      this.descricao = descricao
+      this.id = id
+    }
+  }
+  
+  export default Pratos
+  
