64774077bec79c50f421d3dd3b87b3b39528ab6c
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-07-19 12:08:01 -0300

    Ajuste do diretoro

--- efoods/src/Routes.tsx
+++ efoods/src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 
 import Home from './Pages/Home'
+import Perfil from './Pages/Perfil'
 
 const Rotas = () => (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Perfil" element={<Perfil />} />
     </Routes>
   )
   
